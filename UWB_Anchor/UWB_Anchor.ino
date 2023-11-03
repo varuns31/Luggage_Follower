1999ba276f6d0815c651cb30beab886014fbde10
varuns31 <varun.singhal31@hotmail.com>
2023-11-03 16:52:27 -0500

    Testing UWB

--- UWB_Anchor/UWB_Anchor.ino
+++ UWB_Anchor/UWB_Anchor.ino
@@ -1,5 +1,7 @@
 #include <SPI.h>
-#include "DW1000Ranging.h"
+#include "DW1000Ranging.h"     //https://github.com/thotro/arduino-dw1000
+ 
+#define ANCHOR_ADD "83:17:5B:D5:A9:9A:E2:9C"
  
 #define SPI_SCK 18
 #define SPI_MISO 19
@@ -20,13 +22,20 @@ void setup()
     DW1000Ranging.initCommunication(PIN_RST, PIN_SS, PIN_IRQ); //Reset, CS, IRQ pin
     //define the sketch as anchor. It will be great to dynamically change the type of module
     DW1000Ranging.attachNewRange(newRange);
-    DW1000Ranging.attachNewDevice(newDevice);
+    DW1000Ranging.attachBlinkDevice(newBlink);
     DW1000Ranging.attachInactiveDevice(inactiveDevice);
     //Enable the filter to smooth the distance
     //DW1000Ranging.useRangeFilter(true);
  
-    //we start the module as a tag
-    DW1000Ranging.startAsTag("7D:00:22:EA:82:60:3B:9C", DW1000.MODE_LONGDATA_RANGE_LOWPOWER);
+    //we start the module as an anchor
+    // DW1000Ranging.startAsAnchor("82:17:5B:D5:A9:9A:E2:9C", DW1000.MODE_LONGDATA_RANGE_ACCURACY);
+ 
+    DW1000Ranging.startAsAnchor(ANCHOR_ADD, DW1000.MODE_LONGDATA_RANGE_LOWPOWER, false);
+    // DW1000Ranging.startAsAnchor(ANCHOR_ADD, DW1000.MODE_SHORTDATA_FAST_LOWPOWER);
+    // DW1000Ranging.startAsAnchor(ANCHOR_ADD, DW1000.MODE_LONGDATA_FAST_LOWPOWER);
+    // DW1000Ranging.startAsAnchor(ANCHOR_ADD, DW1000.MODE_SHORTDATA_FAST_ACCURACY);
+    // DW1000Ranging.startAsAnchor(ANCHOR_ADD, DW1000.MODE_LONGDATA_FAST_ACCURACY);
+    // DW1000Ranging.startAsAnchor(ANCHOR_ADD, DW1000.MODE_LONGDATA_RANGE_ACCURACY);
 }
  
 void loop()
@@ -46,9 +55,9 @@ void newRange()
     Serial.println(" dBm");
 }
  
-void newDevice(DW1000Device *device)
+void newBlink(DW1000Device *device)
 {
-    Serial.print("ranging init; 1 device added ! -> ");
+    Serial.print("blink; 1 device added ! -> ");
     Serial.print(" short:");
     Serial.println(device->getShortAddress(), HEX);
 }
